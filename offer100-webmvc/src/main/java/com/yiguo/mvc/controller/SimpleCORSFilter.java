ba95d7a3d1755e29115b38590e00211242dbc5fa
wanghuan <1360527082@qq.com>
2018-03-06 16:46:19 +0800

    处理跨域put的bug

--- offer100-webmvc/src/main/java/com/yiguo/mvc/controller/SimpleCORSFilter.java
+++ offer100-webmvc/src/main/java/com/yiguo/mvc/controller/SimpleCORSFilter.java
@@ -10,9 +10,9 @@ public class SimpleCORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET,PUT,HEAD, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, HEAD, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type,Access-Token");
         chain.doFilter(req, res);
     }
     public void init(FilterConfig filterConfig) {}
