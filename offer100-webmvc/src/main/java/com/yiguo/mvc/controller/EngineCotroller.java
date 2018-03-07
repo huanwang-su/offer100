acbdbf353c922b0ab10d725adc23f884e660592a
zhuifengnian <286311613@qq.com>
2018-03-07 14:19:43 +0800

    3月7日第一次交

--- offer100-webmvc/src/main/java/com/yiguo/mvc/controller/EngineCotroller.java
+++ offer100-webmvc/src/main/java/com/yiguo/mvc/controller/EngineCotroller.java
@@ -1,24 +1,32 @@
 package com.yiguo.mvc.controller;
 
 import com.sun.javafx.fxml.expression.Expression;
+import freemarker.template.Template;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.collections.map.HashedMap;
 import org.apache.tomcat.util.bcel.classfile.Constant;
 import org.omg.IOP.Encoding;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -60,42 +68,70 @@ public class EngineCotroller {
         }
     }
     public static String TimeDifference(long start, long end) {
-
+        String f="true";
         long between = end - start;
         long day = between / (24 * 60 * 60 * 1000);
         long hour = (between / (60 * 60 * 1000) - day * 24);
         long min = ((between / (60 * 1000)) - day * 24 * 60 - hour * 60);
-        long s = (between / 1000 - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);
-        long ms = (between - day * 24 * 60 * 60 * 1000 - hour * 60 * 60 * 1000
-                - min * 60 * 1000 - s * 1000);
-        String timeDifference = day + "天" + hour + "小时" + min + "分" + s + "秒" + ms
-                + "毫秒";
-        return timeDifference;
+        if(day>0)
+            f="false";
+        else if(hour>1)
+            f="false";
+        else if(min>30)
+            f="false";
+
+        return f;
     }
+
     @ApiOperation(value = "用户找回密码",notes = "")
     @ResponseBody
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public void sendEnterpriseSimpleMail(@PathVariable  String email,@PathVariable Integer id ) throws Exception {
-        SimpleMailMessage message = new SimpleMailMessage();
-        String url="Https://yiguo.com/password/reset?";
-         String emailencode=MD5(email);
-        message.setFrom("15337100703@163.com");
-        message.setTo(email);
-        message.setSubject("主题：找回密码邮件");
-        message.setText(url+emailencode+"email="+email+"&"+new Date());
+    @RequestMapping(value = "/sendTemplateMail", method = RequestMethod.GET)
+    public void sendTemplateMail(@PathVariable  String email){
+        String emailencode=MD5("286311613@qq.com")+"&";
+        String url="https://yiguo.com/password/reset?";
 
+        MimeMessage message = null;
+        try {
+            message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(Sender);
+            helper.setFrom(new InternetAddress(Sender, "offer100", "UTF-8"));
+            helper.setTo(email);
+            helper.setSubject("找回密码邮件");
+            helper.setText(url+emailencode+"email="+"286311613@qq.com");
+            Map<String, Object> model = new HashedMap();
+            model.put("username", "尊敬的用户，");
+            model.put("text",url+emailencode+"email="+"286311613@qq.com");
+            //修改 application.properties 文件中的读取路径
+            FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
+            configurer.setTemplateLoaderPath("classpath:templates");
+            //读取 html 模板
+            Template template = freeMarkerConfigurer.getConfiguration().getTemplate("mail.html");
+            String html = FreeMarkerTemplateUtils.processTemplateIntoString(template, model);
+            helper.setText(html, true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         mailSender.send(message);
-
     }
     @ApiOperation(value = "用户点击找回密码链接",notes = "")
     @ResponseBody
     @RequestMapping(value = "/getLink", method = RequestMethod.GET)
-    public void getLink(@PathVariable  String link ,@PathVariable Integer id ) throws Exception {
+    public String getLink(@PathVariable  String link ,@PathVariable Integer id ) throws Exception {
+        String reset = "true";
+        Date date = new Date();
+        long time = date.getTime();
+        String[] links = link.split("&");
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date1 = (Date) formatter.parse(links[1]);
+        long time1 = date1.getTime();
+        String f = TimeDifference(time, time1);
+        if (f.equals("false"))
+            reset = "链接失效";
+        else
+            reset = "可以进行重新设定";
 
-      String []links=link.split("&");
-
-
-
+        return reset;
     }
 
 }
