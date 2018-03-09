23726b82d1dab397cf583a8e566ec8561d04eaeb
Luoyanyan0 <35414553+Luoyanyan0@users.noreply.github.com>
2018-03-09 09:12:53 +0800

    luoyanyan

---
+++ offer100-webmvc/src/test/java/com/yiguo/service/ResumeServiceTest.java
@@ -0,0 +1,19 @@
+package com.yiguo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * created by luoyanyan on 2018/03/08
+ */
+public class ResumeServiceTest {
+
+    @Autowired
+    ResumeService resumeService;
+    @Autowired
+    EnterpriseService enterpriseService;
+    @Autowired
+    UserService userService;
+    @Autowired
+    NotificationService notificationService;
+}
+
