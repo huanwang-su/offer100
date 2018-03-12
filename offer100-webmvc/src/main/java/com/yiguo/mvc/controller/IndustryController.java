955be4ef247c67e41012fe88791d58ec3946dc70
zhuifengnian <286311613@qq.com>
2018-03-12 21:45:56 +0800

    最新版3.12修改api

--- offer100-webmvc/src/main/java/com/yiguo/mvc/controller/IndustryController.java
+++ offer100-webmvc/src/main/java/com/yiguo/mvc/controller/IndustryController.java
@@ -1,14 +1,17 @@
 package com.yiguo.mvc.controller;
 
+import com.yiguo.bean.Industry;
+import com.yiguo.bean.Page;
 import com.yiguo.mvc.vo.IndustryVO;
+import com.yiguo.offer100.common.page.PageInfo;
 import com.yiguo.service.IndustryService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import static com.alibaba.dubbo.monitor.MonitorService.FAILURE;
 
 /**
  * 行业web接口
@@ -16,18 +19,46 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @author wanghuan
  * @date 2018-01-26
  */
-@Api("行业web接口")
+@Api("Industry")
 @Controller
 @RequestMapping("/industry")
 public class IndustryController {
     
     @Autowired private IndustryService industryService;
-    @RequestMapping(value="/getAll",method=RequestMethod.GET)
+    /*@RequestMapping(value="/getAll",method=RequestMethod.GET)
     @ApiOperation("获取所有行业")
     @ResponseBody
     public IndustryVO getAllIndustry() {
         return industryService.getAllIndustry(0);
+    }*/
+    @RequestMapping(value="/getParentId/{id}",method=RequestMethod.GET)
+    @ApiOperation("根据父id获取行业")
+    @ResponseBody
+    public PageInfo<Industry> getParentId(@PathVariable Integer id,@RequestParam Integer pageSize,@RequestParam Integer pageNumber) {
+
+    PageInfo<Industry> pageinfo =new PageInfo<Industry>();
+    Industry industry = new Industry();
+    industry.setParentId(id);
+        pageinfo.setPageNum(pageNumber);
+        pageinfo.setPageSize(pageSize);
+        Page page= new Page();
+        page.setPageNumber(pageNumber);
+        page.setPageSize(pageSize);
+ pageinfo.setTotal(industryService.selectCount(industry));
+ pageinfo.setRows(industryService.select(industry,page));
+ return pageinfo;
     }
-    
+    @RequestMapping(value="/{id}",method=RequestMethod.GET)
+    @ApiOperation("根据id获取industry信息")
+    @ResponseBody
+    public Object getIndustry(@PathVariable Integer id) {
+
+      String key=FAILURE;
+      Industry industry =industryService.selectByPrimaryKey(id);
+    if(industry!=null)
+        return industry;
+    else
+      return key;
+    }
 
 }
